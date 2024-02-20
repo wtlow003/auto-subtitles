cf41dfc2cd5a1fee328202f356a261f3e2064017
wtlow003 <wtlow003@suss.edu.sg>
2024-02-20 19:06:38 +0800

    fix(src): remove invalid print statement

--- src/faster-whisper.py
+++ src/faster-whisper.py
@@ -12,14 +12,13 @@ def main():
         cpu_threads=args.cpu_threads,
         num_workers=args.num_workers,
     )
-    result = model.transcribe_stable(
+    result = model.transcribe_stable(  # type: ignore
         audio=args.audio_path,
         verbose=True,
         vad=args.vad,
         vad_threshold=args.vad_threshold,
         # task="translate"
     )
-    print(result.result)
     # adjust max-length per segment to `args.max_length`
     result.split_by_length(max_chars=args.max_length)
     result.to_srt_vtt(
